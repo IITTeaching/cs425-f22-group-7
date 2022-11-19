d5c97f947fc22026cf42c7de6ba2a8614755e9e4
John Stock <jmks1000@gmail.com>
2022-11-19 14:37:35 -0600

    SQL schema update

--- FinalSchema.sql
+++ FinalSchema.sql
@@ -17,11 +17,29 @@ create schema if not exists employee{
     primary key (SSN),
     };
 create schema if not exists account_type{
-
+    type_name varchar(255),
+    interest_rate int(10),
+    monthly_fees int(10),
+    overdraft_allowed bit,
+    overdraft_fee int(10),
+    primary key (type_name)
     };
 create schema if not exists account{
-
+    customer_name varchar(255),
+    account_isType varchar(255),
+    balance_curr int(10),
+    account_id int(10),
+    primary key (account_id),
+    foreign key (account_isType) references account_type
     };
 create schema if not exists transaction{
-
+    type varchar(255),
+    quantity int(10),
+    description varchar(255),
+    transaction_date date,
+    account_from int(10),
+    account_to int(10),
+    status bit,
+    primary key (transaction_date, account_from, account_to),
+    foreign key (account_from, account_to) references account
     };
