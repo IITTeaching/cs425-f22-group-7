3c8047a35be5a2c91c0b7f63eb9e44506389b54c
Jiecheng Mei <jmei18@hawk.iit.edu>
2022-12-03 22:59:05 -0600

    change

--- CS425FinalCode/src/App.java
+++ CS425FinalCode/src/App.java
@@ -2,22 +2,42 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-public class App {
-    private String url = "jdbc:postgresql://localhost:5432/postgres";
-    private String username = "postgres";
-    private String password = "1989830";
 
-    public Connection connect(){
-        Connection conn = null;
-        try{
-            conn = DriverManager.getConnection(url,username,password);
-            System.out.println("Connection established!");
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-        }
-        return conn;
+public class App {
+    private String url;
+    private String username;
+    private String password;
+    private Connection conn;
+
+    public App(){
+        this.url = "jdbc:postgresql://localhost:5432/postgres";
+        this.username = "postgres";
+        this.password = "1989830";
+        // change url and password to connect local sql server
+
+    }
+    public App(String url, String username,String password){
+        this.url = url;
+        this.username = username;
+        this.password = password;
     }
 
+    public void connect(){
+        try{
+            if (url == null && username == null && password == null){
+                throw new Exception("Missing important information");
+            }
+            try{
+                conn = DriverManager.getConnection(url,username,password);
+            }catch (SQLException s){
+                System.out.println(s.getMessage());
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 
-
+    public void closeConn() throws SQLException {
+        conn.close();
+    }
 }
--- CS425FinalCode/src/main.java
+++ CS425FinalCode/src/main.java
@@ -2,16 +2,26 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        boolean control = true;
-        /*My idea is created a connection class and call it when we need it
-         * then we can focus more on the implement other function into our project*/
-        System.out.println("Welcome to CS425-7 Final Project SQL");
-        try{
-            App app = new App();
-            app.connect();
-        }catch(Exception e){
-            System.out.println(e.getMessage());
+        Scanner scan = new Scanner(System.in);
+        String username = null;
+        String password = null;
 
-        }
+        System.out.println("Welcome to CS425-7 Final Project SQL");
+        System.out.println("Please enter the username: ");
+        username = scan.nextLine();
+        System.out.println("Please enter the password: ");
+        password = scan.nextLine();
+
+        //sql part, send username and password to database and check if there is a user in list and return the type
+        //type might be {customer,teller,manager}
+
+        //if type is customer, get and display all the account that user want to access currently
+
+        String[] customer_menu = {"Current balance","Transfer","Withdraw","Deposit","Transaction"};
+        String[] teller_menu = {"Withdrawal", "Deposit", "transfer", "External Transfer"};
+        String[] manager_menu = {"Withdrawal", "Deposit", "transfer", "External Transfer", "Check balance of user", "Transactions of User"};
+
+        
+
     }
 }
