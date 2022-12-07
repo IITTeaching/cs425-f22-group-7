4ba4539df1bc44adba71579737a56e2895208b94
John Stock <jmks1000@gmail.com>
2022-12-06 18:16:25 -0600

    added some imports to main to facilitate sql integration

--- CS425FinalCode/src/main.java
+++ CS425FinalCode/src/main.java
@@ -1,5 +1,9 @@
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class main {
     public static void main(String[] args) {
